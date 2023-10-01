458f34742a9cd395d2830d7f4840e69a87b06994
boddu Rajesh <rajeshboddu333@gmail.com>
2023-10-01 21:05:16 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
       
       </Route> 
 
-      <Route path="/contact" element={<Contact/>} >
+      <Route path="/contact" element={<Contact mode={mode}/>} >
       
       
       </Route> 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Contact() {
+export default function Contact(props) {
   return (
-    <div className='container mx-3'>
+    <div className='container mx-3' style={{backgroundColor:props.mode==='light'?'white':'#13aba5',color:props.mode==='light'?'black':'white' }}>
 
       <h5> you can Contact us at:</h5>
 
